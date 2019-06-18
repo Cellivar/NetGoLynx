7ca02221dd1eb4442e1fdd48826ca014c29a06e1
Cliff <chapman.cliff@gmail.com>
2019-06-18 00:00:01 -0700

    Add animated scroll to specific id

--- NetGoLynx/Models/IndexModel.cs
+++ NetGoLynx/Models/IndexModel.cs
@@ -7,14 +7,17 @@ namespace NetGoLynx.Models
         public IndexModel(
             string suggestedLinkName = "",
             IEnumerable<Redirect> redirects = null,
-            int id = 0)
+            int id = -1)
         {
             LinkName = suggestedLinkName;
             Redirects = redirects;
+            HighlightRedirectId = id;
         }
 
         public string LinkName { get; }
 
         public IEnumerable<Redirect> Redirects { get; }
+
+        public int HighlightRedirectId { get; private set; }
     }
 }
