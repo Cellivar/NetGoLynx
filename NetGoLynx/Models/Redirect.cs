3c76ae5f55ae6672ca772734186ebe836704177f
Cliff <chapman.cliff@gmail.com>
2019-06-18 22:04:08 -0700

    Add description to redirects

--- NetGoLynx/Models/Redirect.cs
+++ NetGoLynx/Models/Redirect.cs
@@ -1,11 +1,28 @@
 ﻿namespace NetGoLynx.Models
 {
+    /// <summary>
+    /// Information for a redirection
+    /// </summary>
     public class Redirect
     {
+        /// <summary>
+        /// The unique ID of this redirect
+        /// </summary>
         public int RedirectId { get; set; }
 
+        /// <summary>
+        /// The target URL to redirect to
+        /// </summary>
         public string Target { get; set; }
 
+        /// <summary>
+        /// The name of the redirect
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// A description of this redirect
+        /// </summary>
+        public string Description { get; set; }
     }
 }
