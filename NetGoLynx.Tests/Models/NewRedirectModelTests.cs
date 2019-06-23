f2835835e0c38590bab4b4ad36c6df277ebdfa5d
Cliff <chapman.cliff@gmail.com>
2019-06-23 11:50:09 -0700

    Add tests for validation, fix bug in validation

--- NetGoLynx.Tests/Models/NewRedirectModelTests.cs
+++ NetGoLynx.Tests/Models/NewRedirectModelTests.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetGoLynx.Models;
 
 namespace NetGoLynx.Tests.Models
 {
     [TestClass]
     public class NewRedirectModelTests
     {
+        private (bool IsValid, List<ValidationResult> Results) TryValidate(object obj)
+        {
+            var context = new ValidationContext(obj, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(obj, context, results, true);
+
+            return (isValid, results);
+        }
+
+        [DataTestMethod]
+        [DataRow("ValidName")]
+        [DataRow("Valid$Name")]
+        [DataRow("Valid\uD83E\uDD8AName")]
+        public void LinkNameIsValid(string name)
+        {
+            var model = new NewRedirectModel(name)
+            {
+                Target = "https://valid.target"
+            };
+            var result = TryValidate(model);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("{}[]")]
+        [DataRow("Invalid Name")]
+        [DataRow("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")]
+        public void LinkNameIsNotValid(string name)
+        {
+            var model = new NewRedirectModel(name)
+            {
+                Target = "https://valid.target"
+            };
+            var result = TryValidate(model);
+
+            Assert.IsFalse(result.IsValid);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            var model = new NetGoLynx.Models.NewRedirectModel()
+            var model = new NewRedirectModel()
             {
                 LinkName = "Name",
                 Description = "Description",
--- NetGoLynx/Data/RegularExpressionNegateAttribute.cs
+++ NetGoLynx/Data/RegularExpressionNegateAttribute.cs
@@ -1,52 +1,18 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
-using System.Text.RegularExpressions;
 
 namespace NetGoLynx.Data
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
-    public class RegularExpressionNegateAttribute : ValidationAttribute
+    public class RegularExpressionNegateAttribute : RegularExpressionAttribute
     {
-        /// <summary>
-        /// Gets the regular expression pattern to use
-        /// </summary>
-        public string Pattern { get; private set; }
-
-        /// <summary>
-        ///     Gets or sets the timeout to use when matching the regular expression pattern (in milliseconds)
-        ///     (-1 means never timeout).
-        /// </summary>
-        public int MatchTimeoutInMilliseconds
-        {
-            get
-            {
-                return _matchTimeoutInMilliseconds;
-            }
-            set
-            {
-                _matchTimeoutInMilliseconds = value;
-                _matchTimeoutSet = true;
-            }
-        }
-
-        private int _matchTimeoutInMilliseconds;
-        private bool _matchTimeoutSet;
-
-        private Regex Regex { get; set; }
-
         /// <summary>
         /// Constructor that accepts the regular expression pattern
         /// </summary>
         /// <param name="pattern">The regular expression to use.  It cannot be null.</param>
         public RegularExpressionNegateAttribute(string pattern)
+            : base(pattern)
         {
-            Pattern = pattern;
-        }
-
-        public override string FormatErrorMessage(string name)
-        {
-            return base.FormatErrorMessage(name);
         }
 
         /// <summary>
@@ -57,47 +23,14 @@ namespace NetGoLynx.Data
         /// <returns><c>true</c> if the given value matches the current regular expression pattern</returns>
         public override bool IsValid(object value)
         {
-            SetupRegex();
+            var result = base.IsValid(value);
 
-            var stringValue = Convert.ToString(value, CultureInfo.CurrentCulture);
-
-            if (string.IsNullOrEmpty(stringValue))
-            {
-                return true;
-            }
-
-            var m = Regex.Match(stringValue);
-
-            // Needs to be an exact match.
-            return !(m.Success && m.Index == 0 && m.Length == stringValue.Length);
+            return !result;
         }
 
-
-        /// <summary>
-        /// Sets up the <see cref="Regex"/> property from the <see cref="Pattern"/> property.
-        /// </summary>
-        /// <exception cref="ArgumentException"> is thrown if the current <see cref="Pattern"/> cannot be parsed</exception>
-        /// <exception cref="InvalidOperationException"> is thrown if the current attribute is ill-formed.</exception>
-        /// <exception cref="ArgumentOutOfRangeException"> thrown if <see cref="MatchTimeoutInMilliseconds" /> is negative (except -1),
-        /// zero or greater than approximately 24 days </exception>
-        private void SetupRegex()
+        public override string FormatErrorMessage(string name)
         {
-            if (Regex == null)
-            {
-                if (string.IsNullOrEmpty(Pattern))
-                {
-                    throw new InvalidOperationException("Invalid regex pattern for RegularExpressionNegative attribute.");
-                }
-
-                if (!_matchTimeoutSet)
-                {
-                    MatchTimeoutInMilliseconds = 2000;
-                }
-
-                Regex = MatchTimeoutInMilliseconds == -1
-                    ? new Regex(Pattern)
-                    : Regex = new Regex(Pattern, default, TimeSpan.FromMilliseconds(MatchTimeoutInMilliseconds));
-            }
+            return ErrorMessage;
         }
     }
 }
