7d596b2a551dae911596a774c09874bfdc3742ca
Cliff <chapman.cliff@gmail.com>
2019-06-22 21:37:12 -0700

    Add unit test project

---
+++ NetGoLynx.Tests/Models/NewRedirectModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetGoLynx.Tests.Models
+{
+    [TestClass]
+    public class NewRedirectModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var model = new NetGoLynx.Models.NewRedirectModel()
+            {
+                LinkName = "Name",
+                Description = "Description",
+                Target = "https://target.com"
+            };
+
+            var redirect = model.ToRedirect();
+
+            Assert.AreEqual("Name", redirect.Name);
+            Assert.AreEqual("Description", redirect.Description);
+            Assert.AreEqual("https://target.com", redirect.Target);
+        }
+
+    }
+}
