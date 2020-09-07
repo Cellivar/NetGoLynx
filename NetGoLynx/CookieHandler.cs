a6e7344eb5245a8ba14ea555c738a20e6a3431b4
Cellivar <cellivar.kangaroorat@gmail.com>
2020-09-07 16:43:41 -0700

    Implement fix for Chrome SameSite cookie changes

---
+++ NetGoLynx/CookieHandler.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace NetGoLynx
+{
+    /// <summary>
+    /// Methods for handling cookie specifics
+    /// </summary>
+    internal static class CookieHandler
+    {
+        /// <summary>
+        /// Modify SameSite parameter iff the destination browser won't handle 'None'.
+        /// </summary>
+        /// <param name="httpContext">The HTTP request context for the cookie.</param>
+        /// <param name="options">The cookie options.</param>
+        public static void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                if (DisallowsSameSiteNone(userAgent))
+                {
+                    options.SameSite = SameSiteMode.Unspecified;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determine if the user agent won't handle SameSite = None properly.
+        /// </summary>
+        /// <param name="userAgent">The user agent to parse</param>
+        /// <returns>True if SameSite=None will cause problems.</returns>
+        /// <remarks>
+        /// This is a user-agent sniffing method that will need to be updated from time tot ime
+        /// </remarks>
+        private static bool DisallowsSameSiteNone(string userAgent)
+        {
+            if (string.IsNullOrEmpty(userAgent))
+            {
+                return false;
+            }
+
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None,
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions,
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
